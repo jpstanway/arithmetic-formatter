eea161242777549637f32edef5cae02a58a1ed19
Jordan S <jpstanway@gmail.com>
2021-05-26 18:59:43 -0700

    structure problems as subarrays

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,25 @@
 def arithmetic_arranger(problems):
+    global width
+    global diff
+    global a
+    global b
+    arranged_problems = []
 
+    for x in problems:
+        items = x.split(" ")
+        op = items[1] + " "
 
+        if len(items[0]) > len(items[2]):
+            a = "  " + items[0]
+            width = len(a)
+            diff = " " * (width - len(items[2]))
+            b = op + diff + items[2]
+        else:
+            b = op + "  " + items[2]
+            width = len(b)
+            diff = " " * (width - len(items[0]))
+            a = "  " + diff + items[0]
+
+        arranged_problems.append([a, b, "-" * (width + 2)])
+    print(arranged_problems)
     return arranged_problems
--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from arithmetic_arranger import arithmetic_arranger
 from unittest import main
 
-
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
 
